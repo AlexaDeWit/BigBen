82c988a0e0349c0dd0bfca4550b8928a27395148
AlexaDeWit <alexa.dewit@gmail.com>
2013-12-10 10:17:28 -0500

    Update shorten.py
    Excelent updates, vry quality, see comments.

--- modules/pubmsg/shorten.py
+++ modules/pubmsg/shorten.py
@@ -20,11 +20,18 @@ class shorten:
 
     def on_pubmsg(self, nick, connection, event):
         message = event.arguments()[0]
-	if message == ".shorten":
-		if(len(self.lastUrls)>0):
-			connection.privmsg(event.target(), self.shortenURL(self.lastUrls[0]))
-	elif message.startswith(".shorten"):
+        #DEPRECATED
+	#if message == ".shorten":
+	#	if(len(self.lastUrls)>0):
+	#		connection.privmsg(event.target(), self.shortenURL(self.lastUrls[0]))
+	
+	
+	#Continually watches chat for URLS, keeping the last one posted in memory
+	if ("http://" in message or "https://" in message):
+		self.lastUrls = re.findall(r'http[s]?://[^\s<>"]+|www\.[^\s<>"]+',message)
+	#output a shortened version of the most recent url
+	#because of the implementation of this function, .shorten url will store the url 
+	#BEFORE the shorten logic, thus allowing you to do it in a single statement
+	if message.startswith(".shorten"):
 		url = message[9:]
 		connection.privmsg(event.target(), self.shortenURL(url))
-	elif ("http://" in message or "https://" in message):
-		self.lastUrls = re.findall(r'http[s]?://[^\s<>"]+|www\.[^\s<>"]+',message)
