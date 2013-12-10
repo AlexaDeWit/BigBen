bcef9bfdb695f03f5016ca94445adffa483bb41c
AlexaDeWit <alexa.dewit@gmail.com>
2013-12-10 10:33:30 -0500

    Update shorten.py
    .shorten url works because url is stored before the shorten logic is exectued. Redundant shorten functionality is not required.

--- modules/pubmsg/shorten.py
+++ modules/pubmsg/shorten.py
@@ -20,11 +20,18 @@ class shorten:
 
     def on_pubmsg(self, nick, connection, event):
         message = event.arguments()[0]
+	#Scans each incoming message for urls, storing the most recent
+	#Allws compounding with .shorten because the url will be parsed and stored before .shorten is
+	#technically stores all the urls from a message(incase of multiple) but the response will only
+	#be of the first url in the most recent message
+	if ("http://" in message or "https://" in message):
+		self.lastUrls = re.findall(r'http[s]?://[^\s<>"]+|www\.[^\s<>"]+',message)
+	#responds to a user beginning a message with .shorten and responds to the request with the first stored url in chat
 	if message == ".shorten":
 		if(len(self.lastUrls)>0):
 			connection.privmsg(event.target(), self.shortenURL(self.lastUrls[0]))
-	elif message.startswith(".shorten"):
-		url = message[9:]
-		connection.privmsg(event.target(), self.shortenURL(url))
-	elif ("http://" in message or "https://" in message):
-		self.lastUrls = re.findall(r'http[s]?://[^\s<>"]+|www\.[^\s<>"]+',message)
+	#DEPRECATED
+	#elif message.startswith(".shorten"):
+	#	url = message[9:]
+	#	connection.privmsg(event.target(), self.shortenURL(url))
+	
