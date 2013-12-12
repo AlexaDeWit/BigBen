8743bacba87adc9db60a6907793994401b87fa3a
AlexaDeWit <alexa.dewit@gmail.com>
2013-12-12 16:16:02 -0500

    Update doge.py
    Fixed excess disc reading and file io

--- modules/pubmsg/doge.py
+++ modules/pubmsg/doge.py
@@ -1,11 +1,13 @@
+import irclib
+import re
 import random
 class doge:
+    def __init__(self):
+	self.responses = []
+	self.dogeFile = open("responses.txt","r")
+	self.responses = dogeFile.read().splitlines()
+	self.dogeFile.close()
     def on_pubmsg(self, nick, connection, event):
         message = event.arguments()[0]
-        source = event.source().split('!')[0]
-
-	if "doge" in message:
-		fortunestxt = open("modules/pubmsg/doge", 'r')
-		fortuneslist = fortunestxt.read().splitlines()
-		response = random.choice(fortuneslist)
-		connection.privmsg(event.target(), response)
+	if("doge" in message):
+		connection.privmsg(event.target(), self.responses[random.randint(0,len(self.responses)-1)])
